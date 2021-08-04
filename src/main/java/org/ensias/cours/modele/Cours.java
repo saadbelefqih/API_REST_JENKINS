37f9f73ffabce6976d670837ad3e3a9ce55514fd
saadbelefqih <saad.belefqih@gmail.com>
2021-08-04 13:35:34 +0100

    update modeles

--- src/main/java/org/ensias/cours/modele/Cours.java
+++ src/main/java/org/ensias/cours/modele/Cours.java
@@ -4,12 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+
 @Entity
 public class Cours {
 	@Id
